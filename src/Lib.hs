2bec29179761317b677cfc90a07a55d5b5c0f679
Cameron Samak <csamak@apache.org>
2018-08-28 13:18:13 -0700

    Add a brittany config and apply brittany to hs source

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,37 @@
-{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
-module Lib
-    ( startApp
-    , app
-    ) where
+{-# LANGUAGE TypeOperators #-}
 
-import Data.Aeson
-import Data.Aeson.TH
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Network.Wai.Logger       (withStdoutLogger)
-import Servant
+module Lib
+  ( startApp
+  , app
+  )
+where
+
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger             ( withStdoutLogger )
+import           Servant
 
 data User = User
-  { userId        :: Int
+  { userId :: Int
   , userFirstName :: String
-  , userLastName  :: String
+  , userLastName :: String
   } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "annieareyouok" :> Get '[PlainText] String
-    :<|> "users" :> Get '[JSON] [User]
+type API
+   = "annieareyouok" :> Get '[ PlainText] String
+    :<|> "users" :> Get '[ JSON] [User]
 
 startApp :: IO ()
 startApp = do
-    withStdoutLogger $ \logger -> do
-        let settings = setPort 1234 $ setLogger logger defaultSettings
-        runSettings settings app
+  withStdoutLogger $ \logger -> do
+    let settings = setPort 1234 $ setLogger logger defaultSettings
+    runSettings settings app
 
 app :: Application
 app = serve api server
@@ -42,6 +45,4 @@ server = return annie :<|> return users
 annie = "https://youtu.be/h_D3VFfhvs4"
 
 users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+users = [User 1 "Isaac" "Newton", User 2 "Albert" "Einstein"]
