284be65b1571114ff33385f23babae8c730cfdcf
Cameron Samak <csamak@apache.org>
2018-08-29 15:53:40 -0700

    Replace the user type with a basic progress type and lookup from a map

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -12,14 +12,14 @@ import           Test.Hspec.Wai.JSON
 
 spec :: Spec
 spec = with (return app) $ do
-  describe "GET /users" $ do
+  describe "GET /progress/id" $ do
     it "responds with 200" $ do
-      get "/users" `shouldRespondWith` 200
-    it "responds with [User]" $ do
-      let
-        users
-          = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"}]"
-      get "/users" `shouldRespondWith` users
+      get "/progress/1" `shouldRespondWith` 200
+    it "responds with Progress" $ do
+      get "/progress/1" `shouldRespondWith` "{\"jobId\":1,\"completed\":5,\"total\":50}"
+      get "/progress/2" `shouldRespondWith` "{\"jobId\":2,\"completed\":3,\"total\":10}" 
+    it "responds with 404 when the job does not exist" $ do
+      get "/progress/3" `shouldRespondWith` 404
 
   describe "the route for checking if annie's state is passable" $ do
     it "confirms that she is indeed ok" $ do
