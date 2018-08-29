0fa34319b901e2dedb1703fce527c5abb9339067
csamak <eufery@gmail.com>
2018-08-29 23:26:06 +0000

    Execute hlint and weeder during tests

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 module LibSpec
   ( spec
@@ -13,16 +12,16 @@ import           Test.Hspec.Wai.JSON
 spec :: Spec
 spec = with (return app) $ do
   describe "GET /progress/id" $ do
-    it "responds with 200" $ do
+    it "responds with 200" $
       get "/progress/1" `shouldRespondWith` 200
     it "responds with Progress" $ do
       get "/progress/1" `shouldRespondWith` "{\"jobId\":1,\"completed\":5,\"total\":50}"
       get "/progress/2" `shouldRespondWith` "{\"jobId\":2,\"completed\":3,\"total\":10}" 
-    it "responds with 404 when the job does not exist" $ do
+    it "responds with 404 when the job does not exist" $
       get "/progress/3" `shouldRespondWith` 404
 
   describe "the route for checking if annie's state is passable" $ do
-    it "confirms that she is indeed ok" $ do
+    it "confirms that she is indeed ok" $
       get "/annieareyouok" `shouldRespondWith` 200
-    it "reminds us that others care about annie too" $ do
+    it "reminds us that others care about annie too" $
       get "/annieareyouok" `shouldRespondWith` "https://youtu.be/h_D3VFfhvs4"
