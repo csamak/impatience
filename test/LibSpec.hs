df8db378271ab10208f4fc1babc26dfa0902f65e
Cameron Samak <eufery@gmail.com>
2018-08-28 23:59:26 -0700

    Remove the explicit hspec main (instead use the preprocessor)

---
+++ test/LibSpec.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+module LibSpec
+  ( spec
+  )
+where
+
+import           Lib                            ( app )
+import           Test.Hspec
+import           Test.Hspec.Wai
+import           Test.Hspec.Wai.JSON
+
+spec :: Spec
+spec = with (return app) $ do
+  describe "GET /users" $ do
+    it "responds with 200" $ do
+      get "/users" `shouldRespondWith` 200
+    it "responds with [User]" $ do
+      let
+        users
+          = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"}]"
+      get "/users" `shouldRespondWith` users
+
+  describe "the route for checking if annie's state is passable" $ do
+    it "confirms that she is indeed ok" $ do
+      get "/annieareyouok" `shouldRespondWith` 200
+    it "reminds us that others care about annie too" $ do
+      get "/annieareyouok" `shouldRespondWith` "https://youtu.be/h_D3VFfhvs4"
