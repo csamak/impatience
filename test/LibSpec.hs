cbe69e04245820ca4904e5328c9fcd1125f694bb
Cameron Samak <csamak@apache.org>
2018-08-30 05:21:13 +0000

    Add basic swagger schema tests

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -4,24 +4,33 @@ module LibSpec
   )
 where
 
-import           Lib                            ( app )
+import           Lib
 import           Test.Hspec
 import           Test.Hspec.Wai
 import           Test.Hspec.Wai.JSON
+import           Data.Proxy
+import           Generic.Random
+import           Test.QuickCheck
+import           Servant.Swagger.Test
 
 spec :: Spec
-spec = with (return app) $ do
-  describe "GET /progress/id" $ do
-    it "responds with 200" $
-      get "/progress/1" `shouldRespondWith` 200
-    it "responds with Progress" $ do
-      get "/progress/1" `shouldRespondWith` "{\"jobId\":1,\"completed\":5,\"total\":50}"
-      get "/progress/2" `shouldRespondWith` "{\"jobId\":2,\"completed\":3,\"total\":10}" 
-    it "responds with 404 when the job does not exist" $
-      get "/progress/3" `shouldRespondWith` 404
+spec = do
 
-  describe "the route for checking if annie's state is passable" $ do
-    it "confirms that she is indeed ok" $
-      get "/annieareyouok" `shouldRespondWith` 200
-    it "reminds us that others care about annie too" $
-      get "/annieareyouok" `shouldRespondWith` "https://youtu.be/h_D3VFfhvs4"
+  describe "Swagger" $
+   context "ToJSON matches ToSchema" $ validateEveryToJSON (Proxy :: Proxy API)
+
+  with (return app) $ do
+    describe "GET /progress/id" $ do
+      it "responds with 200" $ get "/progress/1" `shouldRespondWith` 200
+      it "responds with Progress" $ do
+        get "/progress/1" `shouldRespondWith` "{\"jobId\":1,\"completed\":5,\"total\":50}"
+        get "/progress/2" `shouldRespondWith` "{\"jobId\":2,\"completed\":3,\"total\":10}"
+      it "responds with 404 when the job does not exist" $ get "/progress/3" `shouldRespondWith` 404
+
+    describe "the route for checking if annie's state is passable" $ do
+      it "confirms that she is indeed ok" $ get "/annieareyouok" `shouldRespondWith` 200
+      it "reminds us that others care about annie too" $
+        get "/annieareyouok" `shouldRespondWith` "https://youtu.be/h_D3VFfhvs4"
+
+instance Arbitrary Progress where
+  arbitrary = genericArbitraryU
