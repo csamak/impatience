7eddc42e3bfad467e64173a8c62ed5c585e81b16
Cameron Samak <csamak@apache.org>
2018-09-10 13:14:11 -0700

    Switch from lift to the hedgehog-provided evalIO

--- test/LibTest.hs
+++ test/LibTest.hs
@@ -2,15 +2,13 @@
 module LibTest where
 
 import           Lib
-import           Control.Monad.Trans.Class
-import           Data.Either
+import           Control.Monad
 import           Data.Proxy
 import           Generic.Random
 import           Hedgehog
 import qualified Hedgehog.Gen                  as Gen
 import qualified Hedgehog.Range                as Range
 import qualified Test.QuickCheck               as QC
-import           Test.Tasty.Hedgehog
 import           Test.Tasty.Hspec
 import           Test.Tasty.HUnit               ( (@=?) )
 import           Servant
@@ -18,15 +16,17 @@ import           Servant.Swagger.Test
 
 spec_json = context "ToJSON matches ToSchema" $ validateEveryToJSON (Proxy :: Proxy API)
 
+evalHandler = evalIO . runHandler
+
 hprop_unknownProgresIs404 = property $ do
   num    <- forAll $ Gen.int (Range.linear 3 2000)
-  result <- lift . runHandler $ progress (toInteger num)
+  result <- evalHandler . progress $ toInteger num
   result === Left err404
 
 hprop_knownProgressIsProgress = property $ do
   num    <- forAll $ Gen.int (Range.linear 1 2)
-  result <- lift . runHandler $ progress (toInteger num)
-  assert $ isRight result
+  result <- evalHandler . progress $ toInteger num
+  void . evalEither $ result
 
 unit_annie = annie @=? "https://youtu.be/h_D3VFfhvs4"
 
