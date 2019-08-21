02ac011e755cf6080a1eb6e6760606b7d346a4cf
Cameron Samak <csamak@apache.org>
2019-08-20 17:36:18 -0700

    Hardcode the names of the js files to avoid the fs

--- test/Test/Static/Entries.hs
+++ test/Test/Static/Entries.hs
@@ -1,24 +1,23 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 module Test.Static.Entries where
 
 import           Static.Entries
 import           Codec.Compression.GZip
 import           Control.Monad.IO.Class
-import           Data.ByteString.Lazy.Char8
-import           Data.Text.Encoding
+import           Data.ByteString.Lazy.Char8     ( unpack )
 import           Language.JavaScript.Parser
-import           WaiAppStatic.Storage.Embedded
 import           Network.Wai.Application.Static
 import           Network.Wai.Test
 import           Hedgehog
 import qualified Hedgehog.Gen                  as Gen
 
+-- hardcoded to avoid depending on the filesystem
+jsLocations = ["impatience.js"]
 jsApp = staticApp $(settings jsEntries)
 
 hprop_jsParseable = property $ do
-  jsEntry  <- forAllWith (show . fst) $ Gen.element jsEntries
-  entry    <- liftIO . snd $ jsEntry
-  response <- liftIO $ runSession (req (encodeUtf8 . eLocation $ entry)) jsApp
-  _        <- evalEither $ parse (unpack . decompress $ simpleBody response) "name.js"
-  success
+  jsLocation <- forAll $ Gen.element jsLocations
+  response   <- liftIO $ runSession (req jsLocation) jsApp
+  _          <- evalEither $ parse (unpack . decompress $ simpleBody response) "name.js"
+  length jsLocations === length jsEntries -- make sure the test isn't missing files
   where req e = request $ setPath defaultRequest e
