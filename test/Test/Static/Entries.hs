4da26bc88022fb8d3201f53e61ccb103c16d09b6
Cameron Samak <csamak@apache.org>
2019-08-31 19:31:14 -0700

    Switch to spago from pulp and bower
    Also setup "npm start" for development hot reloading

--- test/Test/Static/Entries.hs
+++ test/Test/Static/Entries.hs
@@ -3,7 +3,6 @@ module Test.Static.Entries where
 
 import           Static.Entries
 import           Codec.Compression.GZip
-import           Control.Monad.IO.Class
 import           Data.ByteString.Lazy.Char8     ( unpack )
 import           Language.JavaScript.Parser
 import           Network.Wai.Application.Static
@@ -17,7 +16,7 @@ jsApp = staticApp $(settings jsEntries)
 
 hprop_jsParseable = property $ do
   jsLocation <- forAll $ Gen.element jsLocations
-  response   <- liftIO $ runSession (req jsLocation) jsApp
+  response   <- evalIO $ runSession (req jsLocation) jsApp
   _          <- evalEither $ parse (unpack . decompress $ simpleBody response) "name.js"
   length jsLocations === length jsEntries -- make sure the test isn't missing files
   where req e = request $ setPath defaultRequest e
