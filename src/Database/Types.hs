3f3d7bb23794c7b5d1bf7776b305a03861f56216
Richmond Chang <changrichmond@gmail.com>
2020-03-28 20:46:38 -0700

    Add Job to data model as parent for tracking progress, and create CRUD endpoints

--- src/Database/Types.hs
+++ src/Database/Types.hs
@@ -3,6 +3,9 @@
 module Database.Types where
 
 import           Data.Int                       ( Int32 )
+import           Data.Text                      ( Text )
+import           Data.Time                      ( UTCTime )
 import           GHC.Generics                   ( Generic )
 
-data Progress = Progress { _jobId, _completed, _total :: Int32 } deriving (Eq, Show, Generic)
+data Progress = Progress { _progress_id, _jobId, _completed, _total :: Int32, _reportedTime :: UTCTime } deriving (Eq, Show, Generic)
+data Job = Job { _job_id :: Int32, _refId :: Text, _name :: Text, _startTime :: UTCTime } deriving (Eq, Show, Generic)
